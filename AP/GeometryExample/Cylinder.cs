686533e0a047f5a2a7d44eb4d810c25caee2f061
Son <sonhad00757@fpt.edu.vn>
2020-05-12 10:48:39 +0700

    Practical 1

---
+++ AP/GeometryExample/Cylinder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AP.PRACTICAL
+{
+    public class Cylinder
+    {
+        private double BaseArea;
+        private double LateralArea;
+        private double TotalArea;
+        private double Voulume;
+        
+        public void Process(double r, double h)
+        {
+            BaseArea = r * r * Math.PI;
+            LateralArea = 2 * Math.PI * r * h;
+            TotalArea = 2 * Math.PI * r * (h + r);
+            Voulume = Math.PI * r * r * h;
+        }
+
+        public void Result()
+        {
+            Console.WriteLine("Base: "+this.BaseArea+" |  Latera: "+this.LateralArea+" |  Total: "+this.TotalArea+" |  Volume: "+this.Voulume);
+        }
+    }
+}
---
+++ AP/GeometryExample/Geometry.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AP.PRACTICAL
+{
+    public class Geometry
+    {
+        public static void Main(string[] args)
+        {
+            Cylinder c = new Cylinder();
+            Console.WriteLine("Enter dimension of Cylinder");
+            Console.WriteLine("Radius: ");
+            double r = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Height: ");
+            double h = Convert.ToDouble(Console.ReadLine());
+            c.Process(r, h);
+            Console.WriteLine("Cylinder characteristics");
+            Console.WriteLine("Radius: " + r);
+            Console.WriteLine("Height: " + h);
+            c.Result();
+        }
+    }
+}
